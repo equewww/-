b476a15f7fd5f9b9078f0693b15d785c96b88325
equewww <dasha200641@gmail.com>
2024-11-15 16:05:13 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,39 @@
+'''Группа учащихся подготовительного отделения сдает выпускные экзамены
+по трем предметам(математика, физика, русский язык). Учащийся, получивший «2», сразу отчисляется.
+Вывести список учащихся, успешно сдавших экзамены, в порядке убывания полученного ими среднего балла по результатам трех экзаменов.'''
+import random
+
+class Exams:
+    def __init__(self, surname):
+        self.surname = surname
+        self.math = random.randint(2, 5)
+        self.physics = random.randint(2, 5)
+        self.russian = random.randint(2, 5)
+
+    def average(self):
+        if self.math == 2 or self.physics == 2 or self.russian == 2:
+            return None
+        return (self.math + self.physics + self.russian) / 3
+
+    def filter(self, students):
+        passed = [student for student in students if student.average() is not None]
+        for i in range(len(passed)):
+            for j in range(len(passed) - 1):
+                if passed[j].average() < passed[j + 1].average():
+                    passed[j], passed[j + 1] = passed[j + 1], passed[j]
+        return passed
+
+surnames = ['Исаев Д. Е.', 'Спиридонова В. Д.', 'Гончарова К. И.', 'Макарова А. А.',
+            'Васильев М. А.', 'Сафонов М. Т.', 'Мещеряков И. М.', 'Майоров Т. А.',
+            'Николаева Е. К.', 'Павлов П. М.', 'Михайлова М. Е.', 'Бочаров М. В.',
+            'Сидоров Е. Ф.', 'Ефимова М. А.', 'Павловская А. Е.', 'Федорова Л. А.',
+            'Дмитриев М. А.', 'Киреев Д. А.', 'Елисеева М. Т.', 'Беляков И. М.']
+
+students = [Exams(surname) for surname in surnames]
+
+exams = Exams("")
+success = exams.filter(students)
+
+print('Успешно сдали:')
+for student in success:
+    print(f"{student.surname}: Средний балл: {student.average():.2f} (Математика: {student.math}, Физика: {student.physics}, Русский: {student.russian})")
---
+++ task2.py
@@ -0,0 +1,27 @@
+''' На основании результатов соревнований по прыжкам в длину (фамилии и результаты трех попыток)
+составить итоговый протокол соревнований, считая, что в зачет идет лучший результат.'''
+import random
+
+class Jump:
+    def __init__(self, surname):
+        self.surname = surname
+        self.first = round(random.uniform(0, 4), 2)
+        self.second = round(random.uniform(0, 4), 2)
+        self.third = round(random.uniform(0, 4), 2)
+
+    def best(self):
+        if self.first <= self.second and self.third <= self.second:
+            return self.second
+        elif self.first >= self.second and self.third <= self.first:
+            return self.first
+        elif self.first <= self.third and self.third >= self.second:
+            return self.third
+
+surnames = ['Исаев Д. Е.', 'Спиридонова В. Д.', 'Гончарова К. И.', 'Макарова А. А.',
+            'Васильев М. А.', 'Сафонов М. Т.', 'Мещеряков И. М.', 'Майоров Т. А.']
+
+athletes = [Jump(surname) for surname in surnames]
+
+print('Протокол соревнований:')
+for athlete in athletes:
+    print(f"{athlete.surname}: Лучший прыжок: {athlete.best():.2f} (Первый: {athlete.first}, Второй: {athlete.second}, Третий: {athlete.third})")

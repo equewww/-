e56c224270eef35b82371bbcbf898b141260d078
equewww <dasha200641@gmail.com>
2024-12-13 15:04:05 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,17 +1,25 @@
 '''В заданной матрице элементы каждой строки расположить в порядке возрастания. Упорядочение элементов строки оформить в виде метода.'''
+
 import random
-k = input('Введите количество строк: ')
-i = input('Введите количество столбцов: ')
-m  = [[random.randint(-100, 100) for _ in range(int(i))] for _ in range(int(k))]
-print('Матрица:')
-for i in m:
-    print(i)
+
+# Функция для упорядочения строк матрицы:
 def sort_m(m):
     for i in m:
         i.sort()
     return m
 
+k = input('Введите количество строк: ')
+i = input('Введите количество столбцов: ')
+
+# Рандомно создаем матрицу:
+m  = [[random.randint(-100, 100) for _ in range(int(i))] for _ in range(int(k))]
+print('Матрица:')
+for i in m:
+    print(i)
+
+# Используем функцию:
 sorted_m = sort_m(m)
+
 print('Упорядоченная матрица:')
 for i in sorted_m:
     print(i)

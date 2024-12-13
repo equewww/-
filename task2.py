cd3330abdf6eabf32c19a60da467d47149656ead
equewww <dasha200641@gmail.com>
2024-12-13 15:04:43 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,8 +1,13 @@
 '''В двух заданных матрицах найти максимальные элементы и поменять их местами. Поиск максимального элемента матрицы оформить в виде метода.'''
-def find_max(m):
-    max_value = m[0][0]
-    max_position = (0, 0)
 
+import random
+
+# Создаем функцию для поиска максимального элемента в матрице
+def find_max(m):
+    max_value = m[0][0] # Значение максимального элемента
+    max_position = (0, 0) # Позиция максимального элемента
+    
+    # Поиск:
     for i in range(len(m)):
         for k in range(len(m[i])):
             if m[i][k] > max_value:
@@ -11,13 +16,14 @@ def find_max(m):
 
     return max_value, max_position
 
+# Создаем функция для перемещения элементов
 def swap(m1, m2):
-    max1_value, max1_position = find_max(m1)
-    max2_value, max2_position = find_max(m2)
-    m1[max1_position[0]][max1_position[1]] = max2_value
-    m2[max2_position[0]][max2_position[1]] = max1_value
+    max1_value, max1_position = find_max(m1) # Ищем максимальный элемент в первой матрице
+    max2_value, max2_position = find_max(m2) # Ищем максимальный элемент во второй матрице
+    m1[max1_position[0]][max1_position[1]] = max2_value # Присваиваем максимальному элементу первой матрицы значение второго максимального
+    m2[max2_position[0]][max2_position[1]] = max1_value # Присваиваем максимальному элементу второй матрицы значение первого максимального 
     
-import random
+# Создаем матрицы:
 k1 = input('Введите количество строк 1 матрицы: ')
 i1 = input('Введите количество столбцов 1 матрицы: ')
 m1  = [[random.randint(-100, 100) for _ in range(int(i1))] for _ in range(int(k1))]
@@ -32,6 +38,7 @@ print('2 матрица:')
 for i2 in m2:
     print(i2)
     
+# Используем функцию замены максимальных элементов
 swap(m1, m2)
 print("1 матрица после замены:")
 for i1 in m1:

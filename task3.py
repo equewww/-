4028658dd0dff0aa90529194f6730bf12c96ef1a
equewww <dasha200641@gmail.com>
2024-12-19 02:01:36 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -6,43 +6,37 @@ import random
 class Exams:
     def __init__(self, name_of_group):
         self.name_of_group = name_of_group
-        if self.name_of_group == 'БФЗ-24-1':
-            for _ in range(20):
-                self.one = 5
-                self.two = 5
-                self.three = 5
-                self.four = 5
-                self.five = 5
+        self.scores = self.generate_scores()
+
+    def generate_scores(self):
+        # Генерируем оценки для 20 студентов
+        if self.name_of_group == 'БФЗ-24-1 ':
+            return [[5] * 5 for _ in range(20)]  # У группы БФЗ-24-1 бывают только пятерки!!
         else:
-            for _ in range(20):
-                self.one = random.randint(2, 4)
-                self.two = random.randint(2, 4)
-                self.three = random.randint(2, 4)
-                self.four = random.randint(2, 4)
-                self.five = random.randint(2, 4)
+            return [[random.randint(2, 4) for _ in range(5)] for _ in range(20)]  # Другие группы получают не больше 4 :)
 
 
+    # Функция для вычисления среднего балла по каждому студенту в группе
     def average(self):
-        for _ in range(20):
-            self.scores = (self.one + self.two + self.three + self.four + self.five) / 5
-        return self.scores
-
-    def rating(self, scores):
-        for i in range(len(scores)):
-            for j in range(len(scores) - 1):
-                if scores[j].average() < scores[j + 1].average():
-                    scores[j], scores[j + 1] = scores[j + 1], scores[j]
-        return scores
+        scores = sum(sum(student_scores) for student_scores in self.scores)
+        return scores / 100 # 20 студентов * 5 экзаменов
 
 
-name_of_groups = ['БФЗ-24-1', 'БМТМ-24-1','БЭН-24-1']
+name_of_groups = ['БФЗ-24-1 ', 'БМТМ-24-1', 'БЭН-24-1 ']
 
-list = [Exams(name_of_group) for name_of_group in name_of_groups]
+# Формируем список групп
+list_of_groups = [Exams(name_of_group) for name_of_group in name_of_groups]
 
-exams = Exams("")
-success = exams.rating(list)
+# Составляем рейтинг при помощи пузырька
+for i in range(len(list_of_groups)):
+    for j in range(len(list_of_groups) - 1):
+        if list_of_groups[j].average() < list_of_groups[j + 1].average():
+            list_of_groups[j], list_of_groups[j + 1] = list_of_groups[j + 1], list_of_groups[j]
 
 
-print('Рейтинг групп:')
-for group in list:
-    print(f"{group.name_of_group}: Средний балл: {group.average():}")
+print('')
+print('       Рейтинг групп:')
+print("Группа         |","Средний балл")
+print('-' * 30)
+for group in list_of_groups:
+    print(group.name_of_group,'     |',group.average())
